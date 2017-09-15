bd62bda5a37433afafa0ef70634fe82418b4f1a6
Simon Wu <siw@microsoft.com>
2017-09-15 16:48:26 +0800

    establish CI

--- scripts/build.js
+++ scripts/build.js
@@ -7,14 +7,12 @@ var glob = require('glob');
 var path = require('path');
 var yaml = require('js-yaml');
 
-var src = 'src/azure-iot-sdk-node';
+var src = 'src/vss-web-extension-sdk';
 var packageMappingFileRelativePath = 'package_service_mapping.json';
 var repoRelativePath = 'repo.json';
 var dest = 'docs-ref-autogen';
-var configPath = 'node2docfx.json';
-var tempConfigPath = '_node2docfx_temp.json';
 var filenameMaxLength = 100;
-var packagesToFilter = ['azure-iot-amqp-base', 'azure-iot-http-base', 'azure-iot-device-ts-samples', 'registry_sample', 'ts-e2e'];
+var packagesToFilter = [];
 
 
 // 1. prepare
@@ -22,29 +20,11 @@ fse.removeSync(dest);
 fse.mkdirpSync(dest);
 
 var tsconfigs = glob.sync(path.join(src, '**/tsconfig.json'));
-var toc = [];
 
 tsconfigs.forEach(function (tsconfig) {
     var packagePath = tsconfig.replace('tsconfig.json', 'package.json');
     generatePackageDoc(packagePath, dest);
 });
-
-var transformedToc = [{name: 'Device', items:[]}, {name: 'Service', items:[]}, {name: 'Common', items:[]}];
-toc.forEach(function(t) {
-    if (t.name.indexOf('e2e') >= 0) {
-        return;
-    }
-    if (t.name.indexOf('device') >= 0) {
-        transformedToc[0].items.push(t);
-    } else if (t.name.indexOf('common') >= 0 || t.name.indexOf('base') >= 0) {
-        transformedToc[2].items.push(t);
-    } else {
-        transformedToc[1].items.push(t);
-    }
-});
-
-transformedToc = JSON.parse(JSON.stringify(transformedToc));
-fs.writeFileSync(dest + '/toc.yml', yaml.safeDump(transformedToc));
 process.exit(0);
 
 function generatePackageDoc(packagePath, dest) {
@@ -52,17 +32,14 @@ function generatePackageDoc(packagePath, dest) {
     var packageName = fse.readJsonSync(packagePath).name;
     if (packagesToFilter.indexOf(packageName) < 0) {
         console.log(packageName);
-        var sourceCodeBasePath = dir + '/src';
+        var sourceCodeBasePath = dir + '/typings';
         if (fse.existsSync(sourceCodeBasePath)) {
             var packageDest = dest + '/' + packageName;
             fse.mkdirpSync(packageDest);
-            child_process.execSync('typedoc --json ' + dir + '/api.json ' + dir + '/src --module commonjs --ignoreCompilerErrors');
+            child_process.execSync('typedoc --json ' + dir + '/api.json ' + dir + '/typings --module commonjs --ignoreCompilerErrors --includeDeclarations --exclude **/node_modules/**');
             var basePath = sourceCodeBasePath.replace(src + '/', '');
-            child_process.execFileSync('node', ['node_modules/type2docfx/dist/main', dir + '/api.json', packageDest, 'repo.json',
-             '--disableAlphabetOrder', '--basePath', basePath]);
-            var subToc = yaml.safeLoad(fs.readFileSync(packageDest + '/toc.yml'));
-            toc.push(subToc[0]);
-            fse.removeSync(packageDest + '/toc.yml');
+            child_process.execFileSync('node', ['node_modules/type2docfx/dist/main', dir + '/api.json', packageDest,
+             '--hasModule', '--basePath', basePath]);
         } else {
             console.log('No source file for ' + packageName);
         }
